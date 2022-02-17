968af6d5a5d5a113f41fe8e377fd1b4a4a2e47e2
Uri <uri555459@gmail.com>
2022-02-17 14:35:20 +0300

    Added tabs

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,27 +43,78 @@ const ready = () => {
 }
 
 // ===================== Accordion ==========================
-const accordions = document.querySelectorAll('.accordion');
+const accordions = document.querySelectorAll('.accordion')
 
-	accordions.forEach(el => {
-		el.addEventListener('click', (e) => {
-			const self = e.currentTarget;
-			const control = self.querySelector('.accordion__control')
-			const content = self.querySelector('.accordion__content')
+accordions.forEach(el => {
+	el.addEventListener('click', (e) => {
+		const self = e.currentTarget
+		const control = self.querySelector('.accordion__control')
+		const content = self.querySelector('.accordion__content')
 
-			self.classList.toggle('open')
+		self.classList.toggle('open')
 
-			// если открыт аккордеон
-			if (self.classList.contains('open')) {
-				control.setAttribute('aria-expanded', true)
-				content.setAttribute('aria-hidden', false)
-				content.style.maxHeight = content.scrollHeight + 'px'
-			} else {
-				control.setAttribute('aria-expanded', false)
-				content.setAttribute('aria-hidden', true)
-				content.style.maxHeight = null
-			}
-		})
+		// если открыт аккордеон
+		if (self.classList.contains('open')) {
+			control.setAttribute('aria-expanded', true)
+			content.setAttribute('aria-hidden', false)
+			content.style.maxHeight = content.scrollHeight + 'px'
+		} else {
+			control.setAttribute('aria-expanded', false)
+			content.setAttribute('aria-hidden', true)
+			content.style.maxHeight = null
+		}
 	})
+})
+
+// ===================== Tabs ==========================
+const tabs = document.querySelector('.tabs')
+const tabsBtn = document.querySelectorAll('.tabs__btn')
+const tabsContent = document.querySelectorAll('.tabs__content')
+
+if (tabs) {
+	tabs.addEventListener('click', (e) => {
+		if (e.target.classList.contains('tabs__btn')) {
+			const tabsPath = e.target.dataset.tabsPath
+			tabsBtn.forEach(el => { el.classList.remove('tabs__btn--active') })
+			document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('tabs__btn--active')
+			tabsHandler(tabsPath)
+		}
+
+		if (e.target.classList.contains('tabs__arrow--prev')) {
+			let activeBtn = document.querySelector('.tabs__btn--active')
+			let activeParent = activeBtn.closest('.tabs__item')
+			let previousParent = activeParent.previousElementSibling
+
+			if (previousParent) {
+				let prevActive = previousParent.querySelector('.tabs__btn')
+				tabsBtn.forEach(el => { el.classList.remove('tabs__btn--active') })
+				prevActive.classList.add('tabs__btn--active')
+
+				let path = prevActive.dataset.tabsPath
+				tabsHandler(path)
+			}
+		}
+
+		if (e.target.classList.contains('tabs__arrow--next')) {
+			let activeBtn = document.querySelector('.tabs__btn--active')
+			let activeParent = activeBtn.closest('.tabs__item')
+			let nextParent = activeParent.nextElementSibling
+
+			if (nextParent) {
+				let nextActive = nextParent.querySelector('.tabs__btn')
+				tabsBtn.forEach(el => { el.classList.remove('tabs__btn--active') })
+				nextActive.classList.add('tabs__btn--active')
+
+				let path = nextActive.dataset.tabsPath
+				tabsHandler(path)
+			}
+		}
+	});
+}
+
+const tabsHandler = (path) => {
+	tabsContent.forEach(el => { el.classList.remove('tabs__content--active') })
+	document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs__content--active')
+};
 
 document.addEventListener("DOMContentLoaded", ready)
